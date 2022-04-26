ef4c83e996dbe1e67a226a0f7f9d9230700946c6
NaamaOmer <92688650+NaamaOmer@users.noreply.github.com>
2022-04-26 19:12:23 +0300

    Add files via upload

---
+++ Creature.py
@@ -0,0 +1,52 @@
+
+"""---------------------------Creature-class-----------------------------------------------------------------"""
+
+
+class Creature:
+    """
+    this class represents a single creature.
+    """
+
+    def __init__(self, index):
+        """
+        init creature - done in steps.
+        :param: index: the ID which is the index in the array.
+        """
+        self.index = index
+        self.state = 0
+        self.is_fast = False
+        self.x = -1
+        self.y = -1
+        self.generations = 1
+
+    def set_x_y(self, x, y):
+        """
+        x and y coordinates for spot
+        :param x: x coordinate
+        :param y: y coordinate
+        :return: none
+        """
+        self.x = x
+        self.y = y
+
+    def set_gen(self, x):
+        """
+        :param: X: the account of generations where the creature will be sick
+        :return:
+        """
+        self.generations = x
+
+    def get_state(self):
+        """
+        state is heath situation.
+        :return: health state.
+        """
+        return self.state
+
+    def get_x_y(self):
+        """
+        gets the coordinates.
+        :return: x, y coordinates.
+        """
+        return self.x, self.y
+
